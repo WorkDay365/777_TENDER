0235d560a9a0443432194ee56ffea18906059b75
AlexDev <365workday@gmail.com>
2023-07-26 18:20:15 +0200

    First commit

---
+++ server/index.js
@@ -0,0 +1,40 @@
+import express  from "express"
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+import cors from "cors"
+
+const app = express()
+dotenv.config()
+
+// Constants
+const OUR_PORT = process.env.PORT || 3000
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD 
+const DB_NAME = process.env.DB_NAME
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    return res.json({message:'All is fine...'})
+})
+
+async function start(){
+
+    try{
+        await mongoose.connect(
+            `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.svjbwso.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
+        )
+        app.listen(OUR_PORT, () => {
+            console.log(`Server started on port ${OUR_PORT}`)
+        })
+    } catch(error){
+        console.log(error)
+    }
+
+}
+
+start()
